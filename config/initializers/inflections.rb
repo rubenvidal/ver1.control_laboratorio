45df807493ac3ecd61ebf9b0bc4674f222a04a76
Rubén Vidal <ruben.vidal.castellet@gmail.com>
2011-05-20 11:33:03 +0200

    commit inicial

---
+++ config/initializers/inflections.rb
@@ -0,0 +1,35 @@
+# Be sure to restart your server when you modify this file.
+
+# Add new inflection rules using the following format
+# (all these examples are active by default):
+# ActiveSupport::Inflector.inflections do |inflect|
+#   inflect.plural /^(ox)$/i, '\1en'
+#   inflect.singular /^(ox)en/i, '\1'
+#   inflect.irregular 'person', 'people'
+#   inflect.uncountable %w( fish sheep )
+# end
+
+
+ActiveSupport::Inflector.inflections do |inflect|
+  inflect.plural( /$/, 'es')
+  inflect.plural(/z$/i, 'ces')
+  inflect.plural(/([aeiou])$/i, '\1s')
+
+  inflect.plural(/_(.*)$/, 'es_\1')
+  inflect.plural(/z_(.*)$/, 'ces_\1')
+  inflect.plural(/([aeiou])_(.*)$/, '\1s_\2')
+
+  inflect.singular(/s$/i, '')
+  inflect.singular(/es$/i, '')
+  inflect.singular(/ces$/i, 'z')
+  inflect.singular(/([tj])es$/i, '\1e')
+
+  inflect.singular(/(.*)s_(.*)/i, '\1_\2')
+  inflect.singular(/(.*)nes_(.*)/i, '\1n_\2')
+  inflect.singular(/(.*)les_(.*)/i, '\1l_\2')
+
+  inflect.irregular 'user', 'users'
+  inflect.irregular 'email', 'emails'
+
+  inflect.uncountable %w( pantalones ) # xD
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ControlLaboratorio::Application.config.secret_token = '582c2402821cc0e383636d95b8bf2bb0448f4877802c3278c609a07dd3243676b0e55612be00458843301270bb7ef7380ecb8cd14ed5d9e4e9b04ec6ed988c26'
