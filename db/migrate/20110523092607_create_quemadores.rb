7ea44e43010b042b26a4f9ee94b8d63bfc51f1a2
Juanma Cervera <juanmacervera@gmail.com>
2011-05-24 09:57:58 +0200

    Repaso de migraciones

--- db/migrate/20110523092607_create_quemadores.rb
+++ db/migrate/20110523092607_create_quemadores.rb
@@ -1,6 +1,5 @@
 class CreateQuemadores < ActiveRecord::Migration
   def self.up
-    drop_table :quemadores
     create_table :quemadores do |t|
       t.string :nombre
       t.integer :cantidad_mecheros
